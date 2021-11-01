1787a5c57a08383150ab560ac094a9d19f9e8a74
chamzheng <chamzheng@foxmail.com>
2021-11-01 17:06:13 +0800

    test

--- run_result.sh
+++ run_result.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 touch finished/result.csv
-echo "Sample Name,Scientific Name,Ratio,Accession,Type" >> finished/result.csv
+# echo "Sample Name,Scientific Name,Ratio,Accession,Type" >> finished/result.csv
 
 for file in ./blaster_requests/*
 do
@@ -10,14 +10,17 @@ do
     #echo $org_filename
     singlename=${org_filename##*/}
     filename=${singlename%.*}
-    p_output=`awk-csv-parser --output-separator='|' $file/report.csv  | sed '/^$/d' | cut -d'|' -f2,7 | head -n 4 | tail -n 3 | sort -k2`
+    awk-csv-parser --output-separator='|' $file/report.csv | sed '/^$/d' | cut -d'|' -f2,7 | head -n 4 | tail -n 3 | sort -k2 >> finished/result.csv
     # echo ${p_output//|/,} | sed  "s/^/$filename,/g"
-    echo ${p_output//|/,} | sed  "s/^/$filename,/g" | sed "s/$/,P/g" >> finished/result.csv
+    sed -i "s/^/$filename,/g" finished/result.csv
+    sed -i "s/$/,P/g" finished/result.csv
+    # echo ${p_output//|/,} | sed  "s/^/$filename,/g" | sed "s/$/,P/g" >> finished/result.csv
     ## echo -n '#' > finished/$filename.txt
     ## echo -n $filename'_' >> finished/$filename.txt
     ## head -n 2 $file/report.csv | tail -n 1 | cut -d',' -f3 >> finished/$filename.txt
     ## cat $file/sequence.fasta >> finished/$filename.txt
 done
+
 
 cd finished
 zip -q -r ../result.zip *
